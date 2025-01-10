c2e695c90bd1997720fc8296e2b2673f542d03c2
dwave_it@d-wave.co.kr <dwave_it@d-wave.co.kr>
2025-01-10 10:27:15 +0900

    support 추가

---
+++ application/controllers/Support.php
@@ -0,0 +1,36 @@
+<?php
+defined('BASEPATH') OR exit('No direct script access allowed');
+
+class Article extends CI_Controller {
+    
+    public function __construct(){
+        parent::__construct();
+        
+        $this->load->model('contents_model', 'contentsModel');
+        
+//         if($_SERVER["HTTPS"]!="on"){
+//             $rtnUri = 'https://'.$_SERVER['HTTP_HOST'].$_SERVER['REQUEST_URI'];
+//             header('Location:'.$rtnUri);
+//             exit;
+//         }
+    
+    }
+
+	
+	public function index(){
+	    
+	    $this->list();
+// 	    $this->load->view('/admin/login');
+	}
+	
+	
+	public function call(){
+	    $this->load->view('/support/call');
+	}
+	
+	
+	
+	
+	
+	
+}
