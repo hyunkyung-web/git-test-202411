a1d0ab327a90bd1a4da3dd1e4b56e658f4719f61
dwave_it@d-wave.co.kr <dwave_it@d-wave.co.kr>
2025-01-17 16:21:19 +0900

    message update

--- application/controllers/Member.php
+++ application/controllers/Member.php
@@ -24,25 +24,40 @@ class Member extends CI_Controller {
 // 	    $this->load->view('/admin/login');
 	}
 	
-	public function verify()
-	{
+	public function verify(){
 	    
-	    $this->load->view('/member/verify');
+	    $kakao=[
+	        "client_id"=>'2549f043e46bbd82676b804343560ca2', 
+	        "client_secret"=>'6wttiSwgMRVFTQL4MrxhtXEiTXs3i4En',
+	        "profile_url"=>'https://kapi.kakao.com/v2/user/me',
+	        "redirect_uri"=>'http://localhost/member/kakao_callback',
+	        "state"=>md5(mt_rand(111111, 999999))
+	    ];
+	    
+	    $login_auth_url = 'https://kauth.kakao.com/oauth/authorize?response_type=code&client_id='.$kakao["client_id"].'&redirect_uri='.$kakao["redirect_uri"].'&state='.$kakao["state"].'&prompt=login';
+	    $login_token_url = 'https://kauth.kakao.com/oauth/token?grant_type=authorization_code&client_id='.$kakao["client_id"].'&redirect_uri='.$kakao["redirect_uri"].'&client_secret='.$kakao["client_secret"].'&code='.$kakao["code"];
+
+	    $viewData = [
+	       "login_auth_url"=>$login_auth_url, "login_token_url"=>$login_token_url
+	    ];
+	    
+	    $this->load->view('/member/verify', $viewData);
 	}
 	
-	public function signup()
-	{
+	public function kakao_callback(){
+	    
+	}
+	
+	public function signup(){
 	    
 	    $this->load->view('/member/signup');
 	}
 	
-	public function term()
-	{
+	public function term(){
 	    
 	    $this->load->view('/member/term');
 	}
-	public function privacy()
-	{
+	public function privacy(){
 	    
 	    $this->load->view('/member/privacy');
 	}
