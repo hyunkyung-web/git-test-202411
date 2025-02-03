214d655f5d0d1e21438920a981f3f85651757a3c
dwave_it@d-wave.co.kr <dwave_it@d-wave.co.kr>
2025-02-03 18:34:46 +0900

    인증화면 업데이트

--- application/views/member/verify.php
+++ application/views/member/verify.php
@@ -1,128 +1,96 @@
 <!DOCTYPE html>
 <html lang="ko">
-
 <head>
     <meta charset="utf-8" />
-    <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1" />
-    <meta http-equiv="Cache-Control" content="no-cache" />
-    <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1, user-scalable=no"" />
-    <title>회원인증</title>
-    <meta property=" og:author" content="d'wave">
-    <meta property="og:type" content="website">
-    <meta property="og:title" content="닥터웨이브" />
-    <meta property="og:description" content="닥터웨이브" />
-    <meta property="og:image" content="https://dr-wave.co.kr/public/images/logo.png" />
-    <meta property="og:url" content="" />
-    <link rel="icon" type="image/png" href="/public/common/css/logo_ics.png" />
-    <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.15.3/css/all.css" integrity="sha384-SZXxX4whJ79/gErwcOYf+zWLeJdY/qpuqC4cAa9rOGUstPomtqpuNWT9wdPEn2fk" crossorigin="anonymous" />
+	<meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1" />
+	<meta http-equiv="Cache-Control" content="no-cache" />
+	<meta name="viewport" content="width=device-width, initial-scale=1.0" />
+	<title>회원인증</title>
+	<meta property="og:author" content="d'wave">
+	<meta property="og:type" content="website">
+	<meta property="og:title" content="닥터웨이브" />
+	<meta property="og:description" content="닥터웨이브" />
+	<meta property="og:image" content="https://dr-wave.co.kr/public/images/logo.png"/>
+	<meta property="og:url" content="" />
+	<link rel="icon" type="image/png" href="/public/common/css/logo_ics.png" />
+	<link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.15.3/css/all.css" integrity="sha384-SZXxX4whJ79/gErwcOYf+zWLeJdY/qpuqC4cAa9rOGUstPomtqpuNWT9wdPEn2fk" crossorigin="anonymous" />
     <link href="https://fonts.googleapis.com/css2?family=Noto+Sans+KR&family=Roboto&display=swap" rel="stylesheet" />
-    <link rel="stylesheet" href="/public/common/css/user/main.css" />
+    <link rel="stylesheet" href="/public/common/css/user/main.css" />    
     <link rel="stylesheet" href="/public/common/css/fontawesome.all.min.css" />
     <script type="text/javascript" src="/public/common/js/jquery-3.6.0.min.js"></script>
     <script type="text/javascript" src="/public/common/js/jquery-1.7.2.min.js"></script>
     <script type="text/javascript" src="/public/common/js/jquery.touchSwipe.min.js"></script>
     <script type="text/javascript" src="/public/common/js/jquery-ui.min.js"></script>
-    <script type="text/javascript" src="/public/common/js/jquery.ui.touch-punch.min.js"></script>
+    <script type="text/javascript" src="/public/common/js/jquery.ui.touch-punch.min.js"></script>    
     <script src="/public/common/script/fontawesome.all.min.js"></script>
     <script type="text/javascript" src="/public/common/script/main.js"></script>
-<<<<<<< HEAD
     <script type="text/javascript" src="/public/common/script/common.js"></script>
-=======
->>>>>>> branch 'main' of https://github.com/hyunkyung-web/git-test-202411.git
 
-    <!-- Google Tag Manager -->
-    <script>
-    (function(w, d, s, l, i) {
-        w[l] = w[l] || [];
-        w[l].push({
-            'gtm.start': new Date().getTime(),
-            event: 'gtm.js'
-        });
-        var f = d.getElementsByTagName(s)[0],
-            j = d.createElement(s),
-            dl = l != 'dataLayer' ? '&l=' + l : '';
-        j.async = true;
-        j.src =
-            'https://www.googletagmanager.com/gtm.js?id=' + i + dl;
-        f.parentNode.insertBefore(j, f);
-    })(window, document, 'script', 'dataLayer', 'GTM-5Z935QD');
-    </script>
+	<!-- Google Tag Manager -->
+    <script>(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+    new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+    j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+    'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+    })(window,document,'script','dataLayer','GTM-5Z935QD');</script>
     <!-- End Google Tag Manager -->
 </head>
 <!-- Google tag (gtag.js) -->
 <script async src="https://www.googletagmanager.com/gtag/js?id=G-NMXDBRXH4Y"></script>
 <script>
-window.dataLayer = window.dataLayer || [];
-
-function gtag() {
-    dataLayer.push(arguments);
-}
-gtag('js', new Date());
-gtag('config', 'G-NMXDBRXH4Y');
-
-gtag('event', 'screen_view', {
-    'app_name': 'dr-wave.co.kr',
-    'screen_name': 'Home'
-});
-
-
+	window.dataLayer = window.dataLayer || [];
+  	function gtag(){dataLayer.push(arguments);}
+  	gtag('js', new Date());	
+  	gtag('config', 'G-NMXDBRXH4Y');
+  
+	gtag('event', 'screen_view', {
+		'app_name': 'dr-wave.co.kr',
+		'screen_name': 'Home'
+	});
+	
+	
 // 	$(function(){
 // 		$("#btnGtag").click(function(){
 // 			gtag('event', 'click_GTAG_BTN');
 // 		});
 // 	});
+	
+  
 </script>
 
-<body>
+<body>    
     <!-- Google Tag Manager (noscript) -->
-    <noscript><iframe src="https://www.googletagmanager.com/ns.html?id=GTM-5Z935QD" height="0" width="0" style="display:none;visibility:hidden"></iframe></noscript>
+    <noscript><iframe src="https://www.googletagmanager.com/ns.html?id=GTM-5Z935QD"
+    height="0" width="0" style="display:none;visibility:hidden"></iframe></noscript>
     <!-- End Google Tag Manager (noscript) -->
 
-    <?php include_once APPPATH . 'views/header.php'; ?>
+    <?php include_once APPPATH.'views/header.php'; ?>
 
     <div class="member-wrap">
         <div class="container">
             <div class="member_logo">
                 <h1>회원인증</h1>
             </div>
-
-            <div class="member-container">
-<<<<<<< HEAD
+            
+            <div class="member-container"> 
                 <form id="frm1">
 <!--                     <div class="form_item user" id="divName"> -->
 <!--                         <input type="text" id="name" name="name" placeholder="이름" class="input" value="" maxlength="10"/> -->
 <!--                     </div> -->
-=======
-                <form action="#">
-                    <!-- <div class="form_item user" id="divName">
-                        <input type="text" id="name" name="name" placeholder="이름" class="input" value="" maxlength="10" />
-                    </div> -->
->>>>>>> branch 'main' of https://github.com/hyunkyung-web/git-test-202411.git
                     <div class="form_item phone" id="divPhoneNo">
-<<<<<<< HEAD
                         <input type="tel" id="cellphone" name="cellphone" placeholder="휴대전화번호(숫자만 입력하세요)" class="input" maxlength="16" onkeyup="javascript: cmmOnNumber(this);"/>
-=======
-                        <input type="tel" id="phoneNo" name="phoneNo" placeholder="휴대전화번호(숫자만 입력하세요)" class="input" maxlength="16" onkeyup="javascript: cmmOnNumber(this);" />
->>>>>>> branch 'main' of https://github.com/hyunkyung-web/git-test-202411.git
                     </div>
-<<<<<<< HEAD
                     <div class="form_item phone" id="divPhoneNo">
                         <input type="tel" id="auth_code" name="auth_code" placeholder="인증번호" class="input" maxlength="6" onkeyup="javascript: cmmOnNumber(this);"/>
                     </div>
                     <button type="button" id="btnRequest" onclick="javascript: requestMemberValid();">인증요청</button>
                     <button type="button" id="btnValid">인증하기</button>
                     <a href="<?php echo $login_auth_url;?>"><button type="button"></button></a>
-=======
-                    <button type="button">인증하기</button>
-                    <a href="<?php echo $login_auth_url; ?>"><button type="button"></button></a>
->>>>>>> branch 'main' of https://github.com/hyunkyung-web/git-test-202411.git
                 </form>
             </div>
         </div>
     </div>
 
-    <?php include_once APPPATH . 'views/footer.php'; ?>
+    <?php include_once APPPATH.'views/footer.php'; ?>
 
-</body>
-
+</body>   
 </html>
